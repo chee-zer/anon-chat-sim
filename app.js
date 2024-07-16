2b9f67cb75aab5c0c0fe5c52a38a9b3878b09e2a
Subhransu Jena <subunc5@gmail.com>
2024-07-16 23:59:36 +0530

    add rate limiter

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
 
 const roomRouter = require("./routes/roomRoutes");
 const verifyRouter = require("./routes/verifyRoutes");
 const AppError = require("./utils/appError");
 
 const app = express();
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, //this is milliseconds so gotta convert it to soo
+  max: 100, //no of req for each ip per windowMs
+});
 
+app.use(limiter);
 app.use(express.json());
 app.use("/verify", verifyRouter);
 app.use("/rooms", roomRouter);
