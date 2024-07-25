74d71cdb44a3f9129662d1010f236eba8ea35107
Subhransu Jena <subunc5@gmail.com>
2024-07-25 22:49:57 +0530

    fix duplicating fixed rooms

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -10,12 +10,20 @@ const catchAsync = (fn) => {
   };
 };
 
-const fixedRooms = ["programming", "art", "music", "maths", "shitposting"];
-fixedRooms.forEach(
-  catchAsync(async (roomName) => {
-    const room = new Room({ name: roomName });
-    await room.save();
-  })
+const createFixedRooms = async () => {
+  const fixedRooms = ["programming", "art", "music", "maths", "shitposting"];
+
+  for (const roomName of fixedRooms) {
+    const existingRoom = await Room.findOne({ name: roomName });
+    if (!existingRoom) {
+      const room = new Room({ name: roomName });
+      await room.save();
+    }
+  }
+};
+
+createFixedRooms().catch((err) =>
+  console.error("Error creating fixed rooms:", err)
 );
 
 roomRouter
