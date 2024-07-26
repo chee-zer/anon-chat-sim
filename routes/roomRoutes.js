ab52078fefffafc502f5883d4a99467f55794d96
Subhransu Jena <subunc5@gmail.com>
2024-07-26 22:18:25 +0530

    add db seeding script

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -4,28 +4,6 @@ const roomController = require("./../controllers/roomController");
 
 const roomRouter = express.Router();
 
-const catchAsync = (fn) => {
-  return (req, res, next) => {
-    fn(req, res, next).catch(next);
-  };
-};
-
-const createFixedRooms = async () => {
-  const fixedRooms = ["programming", "art", "music", "maths", "shitposting"];
-
-  for (const roomName of fixedRooms) {
-    const existingRoom = await Room.findOne({ name: roomName });
-    if (!existingRoom) {
-      const room = new Room({ name: roomName });
-      await room.save();
-    }
-  }
-};
-
-createFixedRooms().catch((err) =>
-  console.error("Error creating fixed rooms:", err)
-);
-
 roomRouter
   .route("/")
   .get(roomController.validateUserCode, roomController.showRooms);
